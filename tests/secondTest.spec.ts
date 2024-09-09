1e5a1b82cadb1be65dedc35138eb7cea258c2c12
gavinchen <chen.gavin@outlook.com>
2024-09-09 16:54:51 -0400

    1st commit

---
+++ tests/secondTest.spec.ts
@@ -0,0 +1,81 @@
+import {test} from "@playwright/test"
+
+test.beforeAll(() => {
+
+})
+
+test.beforeEach(async({page}) => {
+    await page.goto('http://localhost:4200')
+    await page.getByText('Forms').click()
+    await page.getByText('Form Layouts').click()
+})
+
+
+test('locator syntac rules', async({page}) => {
+    // By Tag name
+    page.locator('input')
+
+    //By ID
+    await page.locator('#inputEmail1').click()
+
+    // by Class value
+    page.locator('.shape-rectangle')
+
+    // by Attribute
+    page.locator('[placeholder="Email"]')
+
+    // by Class value(full)
+    page.locator('[class="input-full-width size-medium status-basic shape-rectangle nb-transition"]')
+
+    // combine different selectors
+    page.locator('input[placeholder="Email"][nbinput]')
+
+    // by xPath (NOT Recommend)
+    page.locator('//*[@id="inputEmail1"]')
+
+    // By the partial text match
+    page.locator(':text("Using')
+
+    // By exact text match
+    page.locator(':text-is("Using the Grid")')
+
+})
+
+
+test('User facing locations', async({page}) =>{
+    await page.getByRole('textbox', {name: "Email"}).first().click()
+    await page.getByRole('button', {name: "Sign in"}).first().click()
+
+
+    await page.getByLabel('Email').first().click()
+
+
+    await page.getByPlaceholder('Jane Doe').click()
+
+    await page.getByText('Using the Grid').click()
+
+    await page.getByTestId('SignIn').click()
+
+    await page.getByTitle('IoT Dashboard').click()
+})
+
+test('locating the child element', async({page}) =>{
+
+    await page.locator('nb-card nb-radio :text-is("Option 1")').click()
+
+    await page.locator('nb-card').locator('nb-radio').locator(':text-is("Option 2")').click()
+
+    await page.locator('nb-card').getByRole('button', {name: "Sign in"}).first().click()
+
+    await page.locator('nb-card').nth(3).getByRole('button').click()
+})
+
+test('locating parent element', async({page}) => {
+
+
+    
+
+})
+
+
+
