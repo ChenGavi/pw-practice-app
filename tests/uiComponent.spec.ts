d360a558659d39cd12a3981836ab4784b3eeec03
gavinchen <chen.gavin@outlook.com>
2025-10-14 15:33:10 -0400

    add new file

---
+++ tests/uiComponent.spec.ts
@@ -0,0 +1,57 @@
+import { test, expect } from '@playwright/test'
+
+
+test.beforeEach(async({page}) => {
+    await page.goto('http://localhost:4200/')
+})
+
+
+test.describe('Form Layouts page', () => {
+    test.beforeEach(async({page}) => {
+        await page.getByText('Forms').click()
+        await page.getByText('Form Layouts').click()
+    })
+
+    test('input fields', async({page}) => {
+        const usingTheGridEmailInput = page.locator('nb-card', {hasText: "Using the Grid"}).getByRole('textbox', {name: "email"} )
+
+        await usingTheGridEmailInput.fill('test@test.com')
+        await page.waitForTimeout(2000)     // to see the typed email address
+        await usingTheGridEmailInput.clear()
+
+        await usingTheGridEmailInput.pressSequentially('test2@test.com', {delay: 500})
+
+        // generic assertion
+        const inputValue = await usingTheGridEmailInput.inputValue()
+        expect(inputValue).toEqual('test2@test.com')
+
+        // locator assertion
+        await expect(usingTheGridEmailInput).toHaveValue('test2@test.com')
+    })
+
+    test('radio button', async({page}) => {
+        const usingTheGridForm = page.locator('nb-card', {hasText: "Using the Grid"})
+
+        await usingTheGridForm.getByLabel('Option 1').check({force: true})
+        await usingTheGridForm.getByRole('radio', {name: "Option 2"}).check({force: true})
+
+        const radioStatus = await usingTheGridForm.getByRole('radio', {name: "Option 2"}).isChecked()
+        expect(radioStatus).toBeTruthy()
+
+        await expect(usingTheGridForm.getByRole('radio', {name: "Option 2"})).toBeChecked()
+
+        expect(await usingTheGridForm.getByRole('radio', {name: "Option 1"}).isChecked()).toBeFalsy()
+        expect(await usingTheGridForm.getByRole('radio', {name: "Option 2"}).isChecked()).toBeTruthy()
+    })
+
+})
+
+test('chekboxes', async({page}) => {
+
+    await page.getByText('Modal & Overlays').click()
+    await page.getByText('Toastr').click()
+
+    await page.getByRole('checkbox', {name: "Hide on click"}).click({force: true})
+
+})
+
